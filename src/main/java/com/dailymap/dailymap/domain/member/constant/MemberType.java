b5c95fdca070672abb466d9290a53eab9cf38128
byeongsoon <byeongsoon94@gmail.com>
2022-10-11 11:30:13 +0900

    Member Entity 생성 - MemberType에 어떤 소셜 로그인으로 가입했는지 저장

---
+++ src/main/java/com/dailymap/dailymap/domain/member/constant/MemberType.java
@@ -0,0 +1,18 @@
+package com.dailymap.dailymap.domain.member.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MemberType {
+
+    KAKAO(0,"카카오회원"),
+    NAVER(1,"네이버회원"),
+    GOOGLE(2,"구글회원"),
+    ;
+
+    private Integer id;
+    private String title;
+
+}
