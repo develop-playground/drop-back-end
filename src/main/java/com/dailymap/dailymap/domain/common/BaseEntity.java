119a9905daf4c1732abc002158f122dd05fef7ff
byeongsoon <byeongsoon94@gmail.com>
2022-10-31 16:19:07 +0900

    FIX: JPA Auditing 기능 오류 해결

--- src/main/java/com/dailymap/dailymap/domain/common/BaseEntity.java
+++ src/main/java/com/dailymap/dailymap/domain/common/BaseEntity.java
@@ -3,11 +3,12 @@ package com.dailymap.dailymap.domain.common;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
-@EntityListeners(EntityListeners.class)
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public abstract class BaseEntity extends BaseTimeEntity{
--- src/main/java/com/dailymap/dailymap/domain/common/BaseTimeEntity.java
+++ src/main/java/com/dailymap/dailymap/domain/common/BaseTimeEntity.java
@@ -3,12 +3,13 @@ package com.dailymap.dailymap.domain.common;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@EntityListeners(EntityListeners.class)
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public abstract class BaseTimeEntity {
