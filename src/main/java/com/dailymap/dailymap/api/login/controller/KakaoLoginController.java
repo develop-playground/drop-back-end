013ec6f1f475c294ac064c1161ea5efbbaca2038
byeongsoon <byeongsoon94@gmail.com>
2022-12-07 20:56:57 +0900

    fix: 개행 수정

--- src/main/java/com/dailymap/dailymap/api/login/controller/KakaoLoginController.java
+++ src/main/java/com/dailymap/dailymap/api/login/controller/KakaoLoginController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
-
 public class KakaoLoginController {
 
     private final KakaoLoginService kakaoLoginService;
